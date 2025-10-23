f8d35ab7f86c647d4960c48c6a7e721b97a21407
Matheus Reinaldo <160988681+reinaldo-matheus@users.noreply.github.com>
2025-10-23 15:57:09 +0000

    chore(config): atualiza dependências, tipagens e ajustes no tsconfig para suporte a testes

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -1,13 +1,11 @@
-import { Knex } from 'knex'
-
-declare module 'knex/types/tables' {
+declare module "knex/types/tables" {
   export interface Tables {
     transactions: {
-      id: string
-      title: string
-      amount: number
-      created_at: string
-      session_id?: string
-    }
+      id: string;
+      text: string;
+      amount: number;
+      created_at: string;
+      session_id?: string;
+    };
   }
 }
